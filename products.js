47d19cff6d09d3bedd75a96d50477ee447a9213f
SoftStitchStudio <softstichstudio@gmail.com>
2026-02-10 11:14:11 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: "sunflower",
+    name: "Sunflower Throw Blanket",
+    category: "throw-blankets",
+    price: 5800,
+    image: "assets/images/sunflower.jpg",
+    description: "Handmade crochet throw that becomes a bouquet."
+  },
+  {
+    id: "rose",
+    name: "English Rose Throw Blanket",
+    category: "throw-blankets",
+    price: 4500,
+    image: "assets/images/rose.jpg",
+    description: "Soft floral crochet throw."
+  }
+];
